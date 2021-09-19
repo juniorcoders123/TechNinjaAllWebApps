63714039e159ea8d7f91b92623f08b5dbe17856a
juniorcoders123 <83218545+juniorcoders123@users.noreply.github.com>
2021-09-19 18:34:49 +0530

    Update homepage.py

--- homepage.py
+++ homepage.py
@@ -1,4 +1,9 @@
 import streamlit as st
+import webbrowser
+
+st.set_page_config(page_title='Streamlit Apps | TechNinja - Junior Coders', page_icon='😎' layout = 'wide', initial_sidebar_state = 'expanded')
+
+#all my apps
 import qrcodewebapp as qr
 import recipedemo as rd
 import text_summarizer as ts
@@ -6,18 +11,25 @@ import stockpredictor2 as sp2
 import temp as tp
 import quotegenerator as qgt
 
-st.set_page_config(page_title='Streamlit Apps | Junior Coders', layout = 'centered', initial_sidebar_state = 'expanded')
-
 PAGES = {
-    "QR CODE Generator": qr,
-     "Recipe Teller":rd,
-     "Stock Predictor":sp2,
-     "Text Summarizer":ts,
-     "Insulter Praiser Bot":tp,
-     "Quote Generator":qgt
+    "QR Generator": qr,
+     "Recipe Teller": rd,
+     "Stock Predictor": sp2,
+     "Text Summarizer": ts,
+     "Insulter/Praiser Bot": tp,
+     "Quote Generator": qgt
     }
 
+query_params = st.experimental_get_query_params()
+default = int(query_params["activity"][0]) if "activity" in query_params else 0
+
 st.sidebar.title('All my Apps!')
-selection = st.sidebar.radio("Go to", list(PAGES.keys()))
+selection = st.sidebar.radio("Go to", list(PAGES.keys()), index=default)
+if selection:
+    st.experimental_set_query_params(activity=list(PAGES).index(selection))
 page = PAGES[selection]
 page.app()
+
+visit_polybit_site = st.sidebar.button("Visit Polybit's Site!")
+if visit_polybit_site:
+    webbrowser.open('https://share.streamlit.io/juniorcoders123/polybit-apps-streamlit/main/homepage.py?activity=0')
