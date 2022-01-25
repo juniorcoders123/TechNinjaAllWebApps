75169ba617d3ed01712a5e0a0684e9f30d309f34
juniorcoders123 <83218545+juniorcoders123@users.noreply.github.com>
2022-01-25 11:28:10 +0530

    Update imagetotextwebapp.py

--- imagetotextwebapp.py
+++ imagetotextwebapp.py
@@ -6,7 +6,7 @@ from gtts import gTTS
 def app():
   st.title("Image to Text Web App")
   st.subheader("Developed with 💖 by TechNinja")
-  st.write("Languages Supported[English, French, Spanish, German, Hindi, Bengali]")
+  st.write("Languages Supported[English]")
   uploaded_image = st.file_uploader('Upload Image', type=['png','jpeg','jpg'])
   aud=st.checkbox("Want Audio for Extracted Text ?")
   extracte=st.button("Extract Text From Image")
